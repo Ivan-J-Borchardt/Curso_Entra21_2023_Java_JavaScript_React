a16822eae7cf13c422200b1d577ab58d0c5d7ee1
Ivan Borchardt <ivan.borchardt.cobol@gmail.com>
2024-02-14 21:21:29 +0100

    Exemplos

--- 006-Node/001-Exemplos/012-API REST p6/src/app.js
+++ 006-Node/001-Exemplos/012-API REST p6/src/app.js
@@ -1,14 +1,15 @@
 import express from "express"
 import routes from "./routes.js"
+import cors from "cors"
 
 
 const app = express()
 
 app.use(express.urlencoded({extended:false}))
 app.use(express.json())
+app.use(cors())
 
 app.use(routes)
-
 
 
 
